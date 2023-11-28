aa6d8c4d609f324bc40fa0c14ca8e63bc071f5cd
developer <sofiia.kazantseva@faceit.com.ua>
2023-11-28 15:06:57 +0200

    Done display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
+import {NgFor, NgIf, UpperCasePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {Hero} from "../hero";
 
 @Component({
+  standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+  ],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
